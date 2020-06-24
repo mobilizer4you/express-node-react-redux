bcf85f29e17fbc7707d4ec30f426a891ebd02243
mobilizer4you <d.m.risingdeveloper@gmail.com>
2020-06-24 11:09:21 +0300

    Added comment for 404 page.

--- frontend/src/components/page-not-found.js
+++ frontend/src/components/page-not-found.js
@@ -5,7 +5,7 @@ class PageNotFound extends Component {
   render () {
     return (
       <section>
-        <h2>Page not found</h2>
+        <h2>404 Page not found.</h2>
 
         <p>The page you are looking for does not exists.</p>
       </section>
